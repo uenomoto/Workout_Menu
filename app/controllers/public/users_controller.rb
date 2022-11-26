a72885e9846bea8cc8eee15bdcc9af18493c9f3a
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2022-11-26 04:12:33 +0000

    #18 作業完了しました！

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,18 +1,36 @@
 class Public::UsersController < ApplicationController
-  def show
-  end
 
-  def edit
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def index
   end
-  
-  
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      #flash[:notice] =
+    redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:nickname, :introduction, :prefecture, :email, :password, :is_deleted, :profile_image)
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   
  def get_profile_image(width, height)
   unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    file_path = Rails.root.join('app/assets/images/no_image.jpeg')
     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
     profile_image.variant(resize_to_limit: [width, height]).processed
