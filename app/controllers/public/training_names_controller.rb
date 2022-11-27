6847c335d842b6c469f14bd9cd1a3a634f43e3a9
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2022-11-27 10:48:32 +0000

    #32 トレーニング名新規登録画面追加

--- app/controllers/public/training_names_controller.rb
+++ app/controllers/public/training_names_controller.rb
@@ -1,5 +1,18 @@
 class Public::TrainingNamesController < ApplicationController
+
+
   def new
+   @training_name = TrainingName.new
+  end
+
+  def create
+   @training_name = TrainingName.new(training_name_params)
+   @training_name.user_id = current_user.id
+   if @training_name.save
+    redirect_to training_names_path
+   else
+    render :new
+   end
   end
 
   def index
@@ -10,12 +23,12 @@ class Public::TrainingNamesController < ApplicationController
 
   def edit
   end
-  
-  
+
+
   private
-  
+
   def training_name_params
     params.require(:training_name).permit(:genre_id, :user_id, :name, :introduction)
   end
-  
+
 end
