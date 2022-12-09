ab5ea704a688e9405c9be80b53abaef9f048e3ac
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2022-12-09 13:43:22 +0000

    #56 作業完了しました

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,16 +1,20 @@
 class Admin::PostCommentsController < ApplicationController
+
+
   def index
+    @post_comments = PostComment.order(created_at: :desc).page(params[:page]).per(6)
   end
 
   def show
+
   end
-  
-  
-  
+
+
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:user_id, :impression_id, :comment)
   end
-  
+
 end
