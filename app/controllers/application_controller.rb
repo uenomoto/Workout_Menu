987ec0c9a5009c83e6ebcdb9ccb8e5dc21bbc631
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2022-12-10 16:30:19 +0000

    #60 作業完了しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,33 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+      flash[:notice]="ログインしました"
+      admin_root_path
+      else
+      flash[:notice]="今日もレッツトレーニング！"
+      root_path
+      end
+    end
+
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :admin
+      flash[:notice]="ログアウトしました"
+      new_admin_session_path
+      else
+      flash[:notice]="お疲れ様でした！"
+      about_path
+      end
+    end
+
+
+
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname, :email])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
         devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduction, :prefecture, :email, :password])
     end
 end
